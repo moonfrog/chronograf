c443207e66d7f7b246a55b0a1cf4bb79110f5552
Tim Raymond <tim@timraymond.com>
2016-09-22 12:10:40 -0400

    Make Open() idempotent and required
    Open() wasn't a hard requirement, so it was a little surprising to need
    to use it when creating an enterprise.Client in some circumstances but
    not others. This returns an error when Querying if Open() was not
    called, preventing panics which would otherwise result. Granted, this
    would only be encountered by developers, but I believe a helpful error
    is sometimes more useful than a mysterious panic when making libs that
    devs actually enjoy using. Furthermore, a preflight check to see whether
    dataNodes was initialized makes the Open() method idempotent.

--- errors.go
+++ errors.go
@@ -3,6 +3,7 @@ package mrfusion
 // General errors.
 const (
 	ErrUpstreamTimeout = Error("request to backend timed out")
+	ErrUninitialized   = Error("client uninitialized. Call Open() method")
 )
 
 // Error is a domain error encountered while processing mrfusion requests
